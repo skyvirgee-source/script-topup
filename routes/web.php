4f9d035c001166fd7341f3cbacaa5b4d25306566
skyvirgee-source <skyvirgee@gmail.com>
2026-09-17 11:16:23 +0700

    Connect topup route to controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TopupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,13 +9,11 @@ Route::get('/', function () {
 
 Route::get('/topup', function () {
     return view('topup.index');
-});
+})->name('topup.index');
 
-Route::post('/topup/process', function () {
-    // Tambahkan logika proses top-up di sini
-    return redirect('/topup/success');
-});
+Route::post('/topup/process', [TopupController::class, 'process'])
+    ->name('topup.process');
 
 Route::get('/topup/success', function () {
     return view('topup.success');
-});
+})->name('topup.success');
